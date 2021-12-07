e624cbab3b1abc7e3b0f4a3ec70510b74b62e337
cstimler <charles.ces@gmail.com>
2021-12-07 04:20:34 -0800

    Working on implementing the Firebase Login Features.

--- app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
+++ app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
@@ -1,8 +1,20 @@
 package com.udacity.project4.authentication
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import android.view.LayoutInflater
+import androidx.activity.viewModels
+import androidx.core.content.ContentProviderCompat.requireContext
+import androidx.lifecycle.Observer
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
 import com.udacity.project4.R
+import com.udacity.project4.databinding.ActivityAuthenticationBinding
 
 /**
  * This class should be the starting point of the app, It asks the users to sign in / register, and redirects the
@@ -10,15 +22,89 @@ import com.udacity.project4.R
  */
 class AuthenticationActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_authentication)
-//         TODO: Implement the create account and sign in using FirebaseUI, use sign in using email and sign in using Google
+    companion object {
+        const val TAG = "AuthenticationActivity"
+        const val SIGN_IN_RESULT_CODE = 1001
+    }
 
+    private lateinit var binding: ActivityAuthenticationBinding
+    private val viewModel by viewModels<LoginViewModel>()
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        observeAuthenticationState()
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_authentication)
+//         TODO: Implement the create account and sign in using FirebaseUI, use sign in using email and sign in using Google
+        binding.authButton.setOnClickListener {
+            launchSignInFlow()
+        }
 //          TODO: If the user was authenticated, send him to RemindersActivity
 
 //          TODO: a bonus is to customize the sign in flow to look nice using :
         //https://github.com/firebase/FirebaseUI-Android/blob/master/auth/README.md#custom-layout
 
     }
+
+    private fun launchSignInFlow() {
+
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build(), AuthUI.IdpConfig.GoogleBuilder().build()
+            // can provide additional other ways to sign in
+        )
+
+        // Create and launch sign-in intent.
+
+        startActivityForResult(
+            AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .build(),
+            AuthenticationActivity.SIGN_IN_RESULT_CODE
+        )
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == SIGN_IN_RESULT_CODE) {
+            val response = IdpResponse.fromResultIntent(data)
+            if (resultCode == Activity.RESULT_OK) {
+                // User successfully signed in
+                Log.i(
+                    TAG,
+                    "Successfully signed in user ${FirebaseAuth.getInstance().currentUser?.displayName}!"
+                )
+            } else {
+                Log.i(TAG, "Sign in unsuccessful ${response?.error?.errorCode}")
+            }
+        }
+    }
+
+    private fun observeAuthenticationState() {
+
+        viewModel.authenticationState.observe(this, Observer { authenticationState ->
+            when (authenticationState) {
+                LoginViewModel.AuthenticationState.AUTHENTICATED -> {
+
+                    binding.authButton.setOnClickListener {
+                        // implement logging out user
+                        AuthUI.getInstance().signOut(applicationContext)
+                    }
+                   // binding.welcomeText.text = getFactWithPersonalization(factToDisplay)
+                }
+                // If there is no logged in user, authButton should display Login and launch the sign
+                //  in screen when clicked. There should also be no personalization of the message
+                //  displayed.
+
+                else -> {
+
+                    binding.authButton.setOnClickListener { launchSignInFlow() }
+
+                }
+            }
+
+        })
+    }
+
+
 }
